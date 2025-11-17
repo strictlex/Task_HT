a5921acb5b11bda195f6846391a217aec2206a7f
strictlex <pislegin2688@gmail.com>
2025-11-17 15:46:12 +0400

    фикс

--- Task2/task2.py
+++ Task2/task2.py
@@ -7,30 +7,16 @@ f2=input("Введите путь к файлу с координатами то
 
 
 def pos_points(file1, file2):
-    with open(
-        f1,
-        "r",
-        encoding="utf-8",
-    ) as file1:
-        x, y = [
-            int(i) for i in file1.readline().rstrip().split()
-        ]  # координаты центра элипса
+    with open(f1,"r",encoding="utf-8",) as file1:
+        x, y = [int(i) for i in file1.readline().rstrip().split()]  # координаты центра элипса
         r1, r2 = [int(i) for i in file1.readline().rstrip().split()]  # радиусы элипса
 
-    with open(
-        f2,
-        "r",
-        encoding="utf-8",
-    ) as file2:
+    with open(f2,"r",encoding="utf-8",) as file2:
         points = [
-            [int(j) for j in i.split()]
-            for i in [el.strip() for el in file2.readlines()]
-        ]  # список отчек
+            [int(j) for j in i.split()] for i in [el.strip() for el in file2.readlines()]]  # список отчек
         positions = []
     for i in points:
-        answer = (
-            ((i[0] - x) ** 2 / r1**2) + ((i[1] - y) ** 2 / r2**2) - 1
-        )  # формула положения точки относительно элипса
+        answer = (((i[0] - x) ** 2 / r1**2) + ((i[1] - y) ** 2 / r2**2) - 1)  # формула положения точки относительно элипса
         if answer == 0:
             positions.append(f"точка {tuple(i)} лежит на окружности")
         elif answer < 0:
